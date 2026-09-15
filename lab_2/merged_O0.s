c0f94a9e067f1ac4ab543bc1b4b1da61a9117f9b
notsergiu2314 <sergiuadrian_p@yahoo.com>
2026-09-15 11:12:33 +0200

    Compiled code to assembly, optimisations O0 and O3

---
+++ lab_2/merged_O0.s
@@ -0,0 +1,83 @@
+	.file	"merged.c"
+	.text
+	.globl	x
+	.bss
+	.align 8
+	.type	x, @object
+	.size	x, 8
+x:
+	.zero	8
+	.text
+	.globl	func
+	.type	func, @function
+func:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	x(%rip), %rax
+	addq	$1, %rax
+	movq	%rax, x(%rip)
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	func, .-func
+	.section	.rodata
+.LC0:
+	.string	"%ld\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	$1, x(%rip)
+	movq	x(%rip), %rax
+	addq	$3, %rax
+	movq	%rax, x(%rip)
+	movl	$0, %eax
+	call	func
+	movq	x(%rip), %rax
+	movq	%rax, %rsi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04.3) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
---
+++ lab_2/merged_O3.s
@@ -0,0 +1,65 @@
+	.file	"merged.c"
+	.text
+	.p2align 4
+	.globl	func
+	.type	func, @function
+func:
+.LFB23:
+	.cfi_startproc
+	endbr64
+	addq	$1, x(%rip)
+	ret
+	.cfi_endproc
+.LFE23:
+	.size	func, .-func
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.LC0:
+	.string	"%ld\n"
+	.section	.text.startup,"ax",@progbits
+	.p2align 4
+	.globl	main
+	.type	main, @function
+main:
+.LFB24:
+	.cfi_startproc
+	endbr64
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 16
+	movl	$5, %edx
+	movl	$1, %edi
+	xorl	%eax, %eax
+	leaq	.LC0(%rip), %rsi
+	movq	$5, x(%rip)
+	call	__printf_chk@PLT
+	xorl	%eax, %eax
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE24:
+	.size	main, .-main
+	.globl	x
+	.bss
+	.align 8
+	.type	x, @object
+	.size	x, 8
+x:
+	.zero	8
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04.3) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
